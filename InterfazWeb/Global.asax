617c2807240194e3f630769c891384eb17db7063
sergio Jiménez <serjimenezm@hotmail.com>
2019-12-03 14:59:59 -0600

    Borrada la interfaz, MICROSOFT >:c

--- InterfazWeb/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FENIC.MvcApplication" Language="C#" %>
--- Modelo/FacturaElectronica.cs
+++ Modelo/FacturaElectronica.cs
@@ -13,7 +13,7 @@ namespace Modelo
         
         [Display(Name = "Codigo de Actividad")]
         [StringLength(6, MinimumLength = 6, ErrorMessage = "Los codigos de actividad deben tener 6 digitos")]
-        public string CodigoActividad { get; set; };
+        public string CodigoActividad { get; set; }
         public string NumeroConsecutivo { get; set; }
 
 

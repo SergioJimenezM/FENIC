d2d0831ec5b9be8811676914de58222bb766a455
Pedro Pablo Lumbí Álvarez <ppablola-98@hotmail.com>
2019-12-03 17:40:48 -0600

    emisor - en acceso a datos

--- AccesoADatos/OperacionesConEmisor.cs
+++ AccesoADatos/OperacionesConEmisor.cs
@@ -3,11 +3,48 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Data.Entity;
 namespace AccesoADatos
 {
     public class OperacionesConEmisor
     {
+        public void agregar(emisor nuevo)
+        {
+            var db = new Context();
+            db.emisor.Add(nuevo);
+            db.Entry(nuevo).State = EntityState.Added;
+            db.SaveChanges();
+        }
 
+        public emisor buscar(int id)
+        {
+            var db = new Context();
+            return db.emisor.Find(id);
+        }
+
+        public void editar(emisor porEditar)
+        {
+            var db = new Context();
+            var DeLaDB = db.emisor.Find(porEditar.id);
+            DeLaDB.Nombre = porEditar.Nombre;
+            DeLaDB.Identificacion = porEditar.Identificacion;
+            DeLaDB.NombreComercial = porEditar.NombreComercial;
+            DeLaDB.Ubicacion = porEditar.Ubicacion;
+            DeLaDB.Telefono = porEditar.Telefono;
+            DeLaDB.CorreoElectronico = porEditar.CorreoElectronico;
+            DeLaDB.identificacion1 = porEditar.identificacion1;
+            DeLaDB.facturaElectronica = porEditar.facturaElectronica;
+            db.Entry(DeLaDB).State = EntityState.Modified;
+            db.SaveChanges();
+
+        }
+
+        public void eliminar(emisor porBorrar)
+        {
+            var db = new Context();
+            db.emisor.Remove(porBorrar);
+            db.Entry(porBorrar).State = EntityState.Deleted;
+            db.SaveChanges();
+        }
     }
 }

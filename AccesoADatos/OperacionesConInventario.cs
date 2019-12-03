3106e2a00b2c3c328750bb4cf3070be4f18e428a
sergio Jiménez <serjimenezm@hotmail.com>
2019-12-03 16:56:17 -0600

    OperacionesConInventario

---
+++ AccesoADatos/OperacionesConInventario.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+namespace AccesoADatos
+{
+    public class OperacionesConInventario
+    {
+        public void nuevo(inventario nuevo)
+        {
+            var db = new FacturaDigitalEntities1();
+            db.inventario.Add(nuevo);
+            db.Entry(nuevo).State = EntityState.Added;
+            db.SaveChanges();
+        }
+    }
+}
---
+++ LogicaDeNegocio/Inventario.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AccesoADatos;
+namespace LogicaDeNegocio
+{
+    public class Inventario
+    {
+        public void agnadirElemento(inventario nuevoElemento) 
+        {
+            
+        }
+    }
+}

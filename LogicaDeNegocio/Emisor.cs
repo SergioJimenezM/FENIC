3654352b3655b18518f527879ee6290166823e20
Pedro Pablo Lumbí Álvarez <ppablola-98@hotmail.com>
2019-12-03 19:18:55 -0600

    cambios en logica y en web

---
+++ LogicaDeNegocio/Emisor.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AccesoADatos;
+namespace LogicaDeNegocio
+{
+    class Emisor
+    {
+        public void agregar(emisor nuevo)
+        {
+            OperacionesConEmisor op = new OperacionesConEmisor();
+            op.agregar(nuevo);
+        }
+
+        public emisor buscar(int id)
+        {
+            OperacionesConEmisor op = new OperacionesConEmisor();
+            return op.buscar(id);
+        }
+
+        public void editar(emisor PorEditar)
+        {
+            OperacionesConEmisor op = new OperacionesConEmisor();
+            op.editar(PorEditar);
+        }
+
+        public void eliminar(emisor PorEliminar)
+        {
+            OperacionesConEmisor op = new OperacionesConEmisor();
+            op.eliminar(PorEliminar);
+        }
+    }
+}
---
+++ LogicaDeNegocio/Factura.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AccesoADatos;
+namespace LogicaDeNegocio
+{
+    class Factura
+    {
+        public void agregar(facturaElectronica nuevo)
+        {
+            OperacionesConFactura op = new OperacionesConFactura();
+            op.agregar(nuevo);
+        }
+
+        public facturaElectronica buscar(int id)
+        {
+            OperacionesConFactura op = new OperacionesConFactura();
+            return op.buscar(id);
+        }
+
+        public void editar(facturaElectronica PorEditar)
+        {
+            OperacionesConFactura op = new OperacionesConFactura();
+            op.editar(PorEditar);
+        }
+
+        public void eliminar(facturaElectronica PorEliminar)
+        {
+            OperacionesConFactura op = new OperacionesConFactura();
+            op.eliminar(PorEliminar);
+        }
+    }
+}
